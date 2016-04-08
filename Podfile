a14bbc3ddf4dacb437b7e5b1a4f9a592db56fb17
Eric Meyer <emeyer@8thlight.com>
2016-04-08 14:10:44 -0500

    Add Quick and Nimble
     * Include dummy test
     * Install via CocoaPods

---
+++ Podfile
@@ -0,0 +1,7 @@
+source "https://github.com/CocoaPods/Specs.git"
+
+target "PrimeFactorsTests" do
+  use_frameworks!
+  pod "Quick", "~> 0.8.0"
+  pod "Nimble", "3.0.0"
+end
---
+++ PrimeFactorsTests/FooSpec.swift
@@ -0,0 +1,14 @@
+import Quick
+import Nimble
+
+class FooSpec: QuickSpec {
+    override func spec() {
+
+        describe("A sample test") {
+            it("passes") {
+                expect(1).to(equal(1))
+            }
+        }
+
+    }
+}
