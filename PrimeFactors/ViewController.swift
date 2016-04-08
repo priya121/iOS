77b3fb6dc7c84f15e151c3ae049ac11654f1f598
Eric Meyer <emeyer@8thlight.com>
2016-04-08 15:14:10 -0500

    Setup layout

--- PrimeFactors/ViewController.swift
+++ PrimeFactors/ViewController.swift
@@ -1,25 +1,12 @@
-//
-//  ViewController.swift
-//  PrimeFactors
-//
-//  Created by Eric Meyer on 4/8/16.
-//  Copyright © 2016 Eric Meyer. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var numberToFactorTextField: UITextField!
+    @IBOutlet weak var primes: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func generatePrimes() {
+        print("generatePrimes: \(numberToFactorTextField.text)")
     }
-
 
 }
-
